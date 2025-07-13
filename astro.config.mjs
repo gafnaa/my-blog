e6a0c5207b31e1ca317c6589163456cdd4e4b406
gafnaa <gafnaalfaatiha@gmail.com>
2025-07-13 19:55:23 +0700

    fix: tailwind problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx({
       remarkPlugins: [],
-    })
+    }),
+    tailwind(),
   ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
