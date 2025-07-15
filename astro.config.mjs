3280a55cc1f7881457cf80aea7670fd5a92598f2
gafnaa <gafnaalfaatiha@gmail.com>
2025-07-15 15:09:33 +0700

    feat: adding 404 not found page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx({
-      remarkPlugins: [],
-    }),
-    tailwind(),
-  ],
+  integrations: [mdx({
+    remarkPlugins: [],
+  }), tailwind(), react()],
 });
