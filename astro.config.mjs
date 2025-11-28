33567a4f0adbdbb4491c3d9e0ba88a6a68748162
gafnaa <gafnaalfaatiha@gmail.com>
2025-11-28 17:08:24 +0700

    feat: Add CTF notes blog post, implement `rehype-pretty-code` for syntax highlighting, and introduce new global and code block styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,31 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const prettyCodeOptions = {
+  theme: 'github-dark',
+  keepBackground: true,
+  /** @param {any} node */
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    remarkPlugins: [],
-  }), tailwind(), react()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    }),
+    tailwind(),
+    react()
+  ],
 });
